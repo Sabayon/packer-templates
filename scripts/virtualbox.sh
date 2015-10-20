c50427a2f1c83547e67a42744289f4c802a85041
mudler <mudler@dark-lab.net>
2015-10-20 19:40:43 +0200

    [script/virtualbox] accepting licenses

--- scripts/virtualbox.sh
+++ scripts/virtualbox.sh
@@ -3,5 +3,8 @@
 echo "Configuring virtualbox!"
 
 chroot /mnt/sabayon /bin/bash <<'EOF'
-echo 3 | equo i app-emulation/virtualbox-guest-additions
+rsync -av -H -A -X --delete-during "rsync://rsync.at.gentoo.org/gentoo-portage/licenses/" "/usr/portage/licenses/"
+ls /usr/portage/licenses -1 | xargs -0 > /etc/entropy/packages/license.accept
+equo i app-emulation/virtualbox-guest-additions
+rm -rf /etc/entropy/packages/license.accept
 EOF
