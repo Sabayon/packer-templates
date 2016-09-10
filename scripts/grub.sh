016a76fb73b1b8916ef7e014a28be2b8d77b5dda
Ettore Di Giacinto <mudler@users.noreply.github.com>
2016-09-10 21:05:18 +0200

    fix missing grup in vagrant image

--- scripts/grub.sh
+++ scripts/grub.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 chroot /mnt/sabayon /bin/bash <<'EOF'
-equo i "sys-boot/grub"
+ACCEPT_LICENSE=* equo i "sys-boot/grub"
 echo "set timeout=0" >> /etc/grub.d/40_custom
 grub2-install /dev/sda
 grub2-mkconfig -o /boot/grub/grub.cfg
