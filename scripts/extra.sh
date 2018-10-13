64977647b861b01e54db6c03cc1215e5b7d3b48c
Ettore Di Giacinto <mudler@users.noreply.github.com>
2018-10-13 18:33:28 +0200

    Update extra.sh

--- scripts/extra.sh
+++ scripts/extra.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-
+chroot /mnt/sabayon /bin/bash <<'EOF'
 wget https://raw.githubusercontent.com/Sabayon/docker-builder-amd64/master/script/post-upgrade.sh
 bash post-upgrade.sh
 rm post-upgrade.sh
-
+EOF
