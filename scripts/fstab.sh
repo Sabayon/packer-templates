bb8f1bfc594f8ca0f278f1baabeed7d8b9e7155c
Ettore Di Giacinto <mudler@gentoo.org>
2019-01-27 12:15:26 +0100

    Drop noauto from /boot

--- scripts/fstab.sh
+++ scripts/fstab.sh
@@ -3,7 +3,7 @@
 chroot /mnt/sabayon /bin/bash <<'EOF'
 cat > /etc/fstab <<'DATA'
 # <fs>		<mount>	<type>	<opts>		<dump/pass>
-/dev/sda1	/boot	ext2	noauto,noatime	1 2
+/dev/sda1	/boot	ext2	noatime	1 2
 /dev/sda4	/	ext4	noatime		0 1
 /dev/sda3	none	swap	sw		0 0
 DATA
