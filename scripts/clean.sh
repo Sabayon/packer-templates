bf750eb5fd7483427d03a1aad03a5ac1ba7487ff
Ettore Di Giacinto <mudler@users.noreply.github.com>
2019-02-28 21:52:31 +0100

    Update clean.sh

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -5,7 +5,7 @@ equo rm sys-kernel/sabayon-sources sabayon-live sabayon-artwork-isolinux
 
 equo rm app-admin/calamares app-misc/calamares-sabayon-server-base-modules app-misc/calamares-sabayon @X
 
-for c in "media-sound" "gnome-base" "net-wireless" "x11-apps" "x11-libs" "x11-themes" "dev-qt" "kde-frameworks" "media-libs";
+for c in "media-sound" "gnome-base" "x11-apps" "x11-libs" "x11-themes" "dev-qt" "kde-frameworks" "media-libs";
 do
 
     TO_REMOVE=$(equo q list installed -q | grep $c | grep -v virtualbox)
