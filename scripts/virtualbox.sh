3442795505adff120cdc8333c3f1c6619ac84bb1
Ettore Di Giacinto <mudler@users.noreply.github.com>
2019-03-01 21:07:04 +0100

    Rebuild dracut

--- scripts/virtualbox.sh
+++ scripts/virtualbox.sh
@@ -5,4 +5,5 @@ echo "Configuring virtualbox!"
 chroot /mnt/sabayon /bin/bash <<'EOF'
 ACCEPT_LICENSE=* equo i app-emulation/virtualbox-guest-additions#$(uname -r) app-emulation/virtualbox-modules#$(uname -r)
 systemctl enable virtualbox-guest-additions
+sabayon-dracut --rebuild-all
 EOF
